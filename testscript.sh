95855150ffa02b165f2d850316361a66df58e2be
rakentitus <154294496+rakentitus@users.noreply.github.com>
2023-12-24 13:02:35 -0500

    Created testscript.sh

---
+++ testscript.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Calculate the sum via command-line arguments
+# $1 and $2 refers to the first and second argument passed as command-line arguments
+ 
+sum=$(( $1 + $2 ))
+ 
+echo "Sum is: $sum" 
+`apt list --installed` >> softwarelist.txt
